0d338032659d235051da90b1a0331c75eba9c8c6
Marcin <wrx75841@student.wsb.wroclaw.pl>
2024-06-08 19:28:26 +0000

    feat:working_app_ver_0_0_1

---
+++ weather/main/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+import requests
+from .models import Cities
+# Create your views here.
+
+city = "London" 
+
+def weather_app(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=0cddd18021fa8eec7083483450f705da'
+    weather_data = []
+    cities_list = Cities.objects.all()
+    
+    if request.method == 'POST':
+        city = request.POST['city']
+        if city:
+            add_city = Cities.objects.create(city=city)
+            add_city.save()
+            return redirect('/')
+    
+    for city in cities_list:
+        get_weather = requests.get(url.format(city)).json()
+        print(get_weather)
+        
+        weather ={
+            'city': city,
+            'temperature': get_weather['main']['temp'],
+            'description': get_weather['weather'][0]['description'],
+            'icon': get_weather['weather'][0]['icon'],
+        }
+        
+        weather_data.append(weather)
+        
+        context = {'weather_data': weather_data}
+    return render(request, 'weather/weather_page.html', context)
